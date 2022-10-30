853917e3426535e4405c3c066e64d0f219469f95
HernanySimao <hernanysimao123@gmail.com>
2022-10-30 14:51:36 -0300

    rotas e plugin adicionado

---
+++ src/components/block.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>OK</h1>
+</template>
+
+<script>
+export default {
+  name: "block"
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    <block />
+  </h1>
+</template>
+
+<script>
+import block from "@/components/block.vue";
+export default {
+  name: "index",
+  components: {
+    block,
+  },
+};
+</script>
+
+<style>
+</style>
